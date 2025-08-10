77e5555bc44565aacf0b3cd5cff1c78e1231176b
bigmistqke <vandijckv@gmail.com>
2025-08-10 02:50:35 +0200

    docs: update Orthographic stories to modern storybook

--- .storybook/stories/OrthographicCamera.stories.tsx
+++ .storybook/stories/OrthographicCamera.stories.tsx
@@ -1,55 +1,58 @@
 import { For } from 'solid-js'
 import { Canvas } from 'solid-three'
+import type { Meta, StoryObj } from 'storybook-solidjs-vite'
 import { Icosahedron, OrbitControls, OrthographicCamera } from '../../src'
 import { T } from '../t'
 
-export default {
+const meta = {
   title: 'Camera/OrthographicCamera',
   component: OrthographicCamera,
-}
+} satisfies Meta<typeof OrthographicCamera>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+/**********************************************************************************/
+/*                                                                                */
+/*                              OrthographicCamera                               */
+/*                                                                                */
+/**********************************************************************************/
 
 const NUM = 3
 
-interface Positions {
+interface Position {
   id: string
   position: [number, number, number]
 }
 
-function OrthographicCameraScene() {
-  const positions = (() => {
-    const pos: Positions[] = []
+export const Default: Story = {
+  render: () => {
+    const positions: Position[] = []
     const half = (NUM - 1) / 2
 
     for (let x = 0; x < NUM; x++) {
       for (let y = 0; y < NUM; y++) {
-        pos.push({
+        positions.push({
           id: `${x}-${y}`,
           position: [(x - half) * 4, (y - half) * 4, 0],
         })
       }
     }
 
-    return pos
-  })()
-
-  return (
-    <Canvas style={{ height: '100vh' }}>
-      <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={40} />
-      <T.Group position={[0, 0, -10]}>
-        <For each={positions}>
-          {({ position }) => (
-            <Icosahedron position={position} args={[1, 1]}>
-              <T.MeshBasicMaterial color="white" wireframe />
-            </Icosahedron>
-          )}
-        </For>
-      </T.Group>
-      <OrbitControls />
-    </Canvas>
-  )
-}
-
-export const OrthographicCameraSceneSt = () => <OrthographicCameraScene />
-OrthographicCameraSceneSt.story = {
-  name: 'Default',
+    return (
+      <Canvas style={{ height: '100vh' }}>
+        <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={40} />
+        <T.Group position={[0, 0, -10]}>
+          <For each={positions}>
+            {({ position }) => (
+              <Icosahedron position={position} args={[1, 1]}>
+                <T.MeshBasicMaterial color="white" wireframe />
+              </Icosahedron>
+            )}
+          </For>
+        </T.Group>
+        <OrbitControls />
+      </Canvas>
+    )
+  },
 }
